64a0d03c50f53bf23050746d8f6116ad5f892e2d
lion4464 <76605747+lion4464@users.noreply.github.com>
2021-09-20 16:41:25 +0500

    Delete AbstractClasses directory

--- AbstractClasses/TestAbstract.java
+++
@@ -1,6 +0,0 @@
-package AbstractClasses;
-
-public abstract class TestAbstract  implements Comparable,Cloneable{
-
-
-}
--- AbstractClasses/TestObject.java
+++
@@ -1,8 +0,0 @@
-package AbstractClasses;
-
-public class TestObject {
-    public static void main(String[] args) {
-
-    }
-
-}
